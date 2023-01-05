1b0a07814f80e0e2c38e145fd6708b6fd17f6a1f
alsrjs7633 <115600045+alsrjs7633@users.noreply.github.com>
2023-01-05 20:07:01 +0900

    20230105last

--- src/main/java/com/cos/pj/repository/UploadRepository.java
+++ src/main/java/com/cos/pj/repository/UploadRepository.java
@@ -1,8 +1,6 @@
 package com.cos.pj.repository;
 
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
